a8f16eae94d5e3d365cc858bb6c91e4949bf60e4
vvcigy <65108648+vvcigy@users.noreply.github.com>
2020-07-01 18:25:08 +1000

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,19 +1,1 @@
-'use strict';
 
-class User{
-    #name = 'Alex';
-    #surname = 'Tyrickov';
-
-    get name(){
-        return this.#name
-    }
-
-    set name(name){
-        this.#name = name;
-    }
-}
-
-let Alex = new User();
-console.log(Alex.name);
-console.log(Alex.name = 'Ivan');
-console.log(Alex.name);
